f229ef17fb8854c89e02adf7f2d979c582b0294c
LE Thomas <thomas.le.tle@gmail.com>
2024-05-03 01:47:55 +0200

    done

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,6 +1,9 @@
 #include "Array.hpp"
 #include <iostream>
 #include <string>
+#include <ctime>
+#include <cstdlib>
+#define MAX_VAL 750
 
 int main()
 {
@@ -82,5 +85,53 @@ int main()
         std::cout << stringArray[i] << std::endl;
     }
 
+    std::cout << std::endl << RED "---------------------------------------------------"<< std::endl;
+	std::cout <<"TEST GIVEN MAIN:" << std::endl;
+	std::cout << "---------------------------------------------------" RESET << std::endl << std::endl;
+    Array<int> numbers(MAX_VAL);
+    int* mirror = new int[MAX_VAL];
+    srand(time(NULL));
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        const int value = rand();
+        numbers[i] = value;
+        mirror[i] = value;
+    }
+    //SCOPE
+    {
+        Array<int> tmp = numbers;
+        Array<int> test(tmp);
+    }
+
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        if (mirror[i] != numbers[i])
+        {
+            std::cerr << "didn't save the same value!!" << std::endl;
+            return 1;
+        }
+    }
+    try
+    {
+        numbers[-2] = 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    try
+    {
+        numbers[MAX_VAL] = 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        numbers[i] = rand();
+    }
+    delete [] mirror;//
     return (0);
 }
